00fac89cd9fba0dc1985faf9169ca9355b5023d4
Vivek Sahu <viveksahu1762@gmail.com>
2024-12-02 09:20:39 +0530

    done

--- next.config.js
+++ next.config.js
@@ -28,6 +28,7 @@ module.exports = withPWA({
   reloadOnOnline: true,
   swcMinify: true,
   // disable: process.env.NODE_ENV === "development",
+  disable: false,
   workboxOptions: {
     disableDevLogs: true,
   },
